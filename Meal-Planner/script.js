886a91a0235d43755c9c224bb271313128a59a9a
William Mosto <will.mosto@gmail.com>
2021-08-25 16:36:04 -0400

    Optional Onclick

--- Meal-Planner/script.js
+++ Meal-Planner/script.js
@@ -1,68 +1,70 @@
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector('#new_task_description')
     const form = document.querySelector('form')
-        form.addEventListener('submit', (e) => {
-            e.preventDefault()
-            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
-            .then(res => res.json())
-            .then(object => { 
-                // const mealArray = object.meals[0]
-                // createMeals(mealArray)
-                const mealArray = object.meals
-                console.log(mealArray)
-                mealArray.forEach(mealObject => createMeals(mealObject))
-            })
-            .catch(err => {
-            alert('Invalid Meal Request : Please Try Again')
-            console.error('Input Error')
-            })
-            form.reset()
+    form.addEventListener('submit', fetchFood)
+    function fetchFood(e){
+        e.preventDefault()
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+        .then(res => res.json())
+        .then(object => { 
+            const mealArray = object.meals
+            console.log(mealArray)
+            mealArray.forEach(mealObject => createOptions(mealObject))
         })
+        .catch(err => {
+        alert('Please Try A Different Food')
+        console.error('Invalid Meal Request')
+        })
+        form.reset()
+    }
 })
 
-
-
-function createMeals(food) { 
-    
+function createOptions(food) { 
     const li = document.createElement('li')
-   
-    
-    li.addEventListener("click", (e) => {
-        const mealPlan = document.querySelector('#mealPlan')
-        // const mealItem = document.getElementById('mealP')
-        const li2 = document.createElement('li')
-        li2.innerText = food.strMeal
-        if (food.strSource !== "") {
-            const link = document.createElement('a')
-            link.innerText = 'Link to Recipe'
-            link.href = food.strSource
-            li2.append(link)     
-        } else if (food.strSource === "") {
-             const link = document.createElement('a')
-            link.innerText = 'Link to Instructional Video'
-            link.href = food.strYoutube
-            li2.appendChild(link)   
-            }
-        mealPlan.appendChild(li2)
-    })
+    li.addEventListener("dblclick", createPlan)  
     const mealList = document.getElementById('mealO') 
     const img = document.createElement('img')
     li.innerText = food.strMeal
     img.src = food.strMealThumb
-    img.alt = 'Failed To Load'
+    img.alt = 'Image Failed To Load'
     mealList.appendChild(li)
-    
     li.appendChild(img)
-    const btn = document.createElement('button')
-    btn.innerText = "Delete"
-    btn.addEventListener("click", () => {
-      li.remove()
-      li2.remove()
+    const deleteBtn = document.createElement('button')
+    deleteBtn.innerText = "Delete"
+    deleteBtn.addEventListener("click", () => {
+        li.remove()
     })
-    li.append(btn)
+    li.append(deleteBtn)  
 
+function createPlan() {
+    const mealPlan = document.querySelector('#mealPlan')
+    const li2 = document.createElement('li')
+    li2.id = 'li2'
+    li2.innerText = food.strMeal
+    if (food.strSource !== "") {
+        const link = document.createElement('a')
+        link.innerText = 'Link to Recipe'
+        link.href = food.strSource
+        li2.append(link)     
+    } else if (food.strSource === "") {
+        const link = document.createElement('a')
+        link.innerText = 'Link to Instructional Video'
+        link.href = food.strYoutube
+        li2.appendChild(link)   
+    }
+    mealPlan.appendChild(li2)
+}
+function resetButton(){
+    const btn2 = document.createElement('button')
+    const mealPlan = document.querySelector('#mealPlan')
+    const li2 = document.querySelectorAll('li')[1]
+    btn2.innerText = "Reset"
+    btn2.addEventListener('click', () => {
+        li2.remove()
+    })
+    mealPlan.append(btn2)
+}
 
-    
 }
 
 
@@ -87,42 +89,3 @@ function createMeals(food) {
 
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const input = document.querySelector('#new_task_description')
-//     const form = document.querySelector('form')
-//         form.addEventListener('submit', (e) => {
-//             e.preventDefault()
-//             fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${input.value}`, {
-//             "method": "GET",
-//             "headers": {
-//                 "x-rapidapi-key": "745e1b9d46mshf4588f87ded654ep1d07a4jsn9d5fd6af39db",
-//                 "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-//             }
-//             })
-//             .then(res => res.json())
-//             .then(object => { 
-//                 const mealArray = object.results
-//                 console.log(mealArray)
-//                 mealArray.forEach(mealObject => createMeals(mealObject))
-//                 debugger
-//             })
-//             // .catch(err => {
-//             //     alert('Invalid Meal Request')
-//             //     console.error(err)
-//             // })
-//             form.reset()
-//         })
-
-//         function createMeals (food) { 
-//             const li = document.createElement('li')
-//             const mealList = document.getElementById('meals') 
-//             const img = document.createElement('img')
-//             li.innerText = food.title
-//             img.src = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${input.value}${food.image}`
-//             img.alt = 'picture'
-//             mealList.appendChild(li)
-//             li.appendChild(img)
-//             console.log(img)
-//         }
-        
-// })
