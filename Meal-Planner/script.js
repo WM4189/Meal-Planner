d2d6281619dcbfa7c4629473e25a0aa458240d2d
William Mosto <will.mosto@gmail.com>
2021-08-25 18:13:45 -0400

    POST attempt

--- Meal-Planner/script.js
+++ Meal-Planner/script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector('#new_task_description')
     const form = document.querySelector('form')
     form.addEventListener('submit', fetchFood)
+
     function fetchFood(e){
         e.preventDefault()
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
@@ -29,6 +30,17 @@ function createOptions(food) {
     img.alt = 'Image Failed To Load'
     mealList.appendChild(li)
     li.appendChild(img)
+    if (food.strSource !== "") {
+        const link = document.createElement('a')
+        link.innerText = 'Link to Recipe'
+        link.href = food.strSource
+        li.append(link)     
+    } else if (food.strSource === "") {
+        const link = document.createElement('a')
+        link.innerText = 'Link to Instructional Video'
+        link.href = food.strYoutube
+        li.appendChild(link)   
+    }
     const deleteBtn = document.createElement('button')
     deleteBtn.innerText = "Delete"
     deleteBtn.addEventListener("click", () => {
@@ -41,30 +53,43 @@ function createPlan() {
     const li2 = document.createElement('li')
     li2.id = 'li2'
     li2.innerText = food.strMeal
-    if (food.strSource !== "") {
-        const link = document.createElement('a')
-        link.innerText = 'Link to Recipe'
-        link.href = food.strSource
-        li2.append(link)     
-    } else if (food.strSource === "") {
-        const link = document.createElement('a')
-        link.innerText = 'Link to Instructional Video'
-        link.href = food.strYoutube
-        li2.appendChild(link)   
-    }
+    const strMeal = li2.innerText
     mealPlan.appendChild(li2)
-}
-function resetButton(){
+    const p = document.createElement('p')
+    p.innerText = food.strInstructions
+    const strInstructions = p.innerText
     const btn2 = document.createElement('button')
-    const mealPlan = document.querySelector('#mealPlan')
-    const li2 = document.querySelectorAll('li')[1]
-    btn2.innerText = "Reset"
-    btn2.addEventListener('click', () => {
-        li2.remove()
+    btn2.innerText = "Save to DB"
+    btn2.addEventListener('click',() => {
+        const newFood = {
+            strMeal,
+            strInstructions 
+        }
+        const configObj = {
+            method: "POST",
+                headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFood)
+          }
+        
+          fetch('http://localhost:3000/meal',configObj)
+          .then(res => res.json())
+          .then(data => console.log(data))
     })
-    mealPlan.append(btn2)
+    li2.appendChild(p)
+    p.appendChild(btn2)
 }
-
+// function resetButton(){
+//     const btn2 = document.createElement('button')
+//     const mealPlan = document.querySelector('#mealPlan')
+//     const li2 = document.querySelectorAll('li')[1]
+//     btn2.innerText = "Reset"
+//     btn2.addEventListener('click', () => {
+//         li2.remove()
+//     })
+//     mealPlan.append(btn2)
+// }
 }
 
 
